a601499ca8709afa1da2bf06ba0ffd4c8b398dec
rahul-4321m <shahrahul2051@gmail.com>
2026-01-17 15:51:26 +0545

    DB initialization setting and scripts

---
+++ app/core/config.py
@@ -0,0 +1,25 @@
+import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+
+    PROJECT_NAME: str = "AI-Powered Content Moderation System"
+
+    #Database
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+
+    @property
+    def DATABASE_URL(self) -> str:
+        url = os.getenv("DATABASE_URL")
+        if url:
+            return url
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+settings = Settings()
