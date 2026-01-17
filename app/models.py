46fc76fc920bc5825677c3b009a6155cb1e849db
rahul-4321m <shahrahul2051@gmail.com>
2026-01-17 15:49:01 +0545

    DB table model

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, JSON
+from datetime import datetime
+from appp.databse import Base
+
+class ModerationLog(Base):
+    __tablename__ = "moderation_logs"
+
+    id = Column(Integer, promary_key=True, index=True)
+    user_id = Column(String, index=True)
+    content_type = Column(String, 5) #holds value text or image
+    original_content = Column(String)
+    is_safe = Column(Integer) #1 and 0
+    scores = Column(JSON)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class BlockedUser(Base):
+    _tablename__="blocked_users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, unique=True, index=True)
+    reason = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
